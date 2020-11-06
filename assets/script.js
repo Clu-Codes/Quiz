ece4cfed710f8b847b0a4aef211b66f944ce9186
Clu <retsulcconnor@gmail.com>
2020-11-06 15:46:57 -0500

    cleaned up HTML and CSS, created first question using js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var questions = [
     {
-        question: "First Question",
-        answers: ['first answer', 'second answer', 'third answer', 'fourth answer'],
+        question: "What is my dog's name?",
+        answers: ['Bristol', 'London', 'Adelaide', 'Sydney'],
         correct: 'first answer'
     },
     {
@@ -28,44 +28,69 @@ var questions = [
 
 // querySelector for Event Listener to begin quiz
 var startButtonEl = document.querySelector("#begin-quiz");
-var mainDivEl = document.querySelector(".div-main");
+var containerEl = document.querySelector(".container");
+var titleEl = document.querySelector(".title-div");
+var rulesEl = document.querySelector(".rules-div");
+var buttonEl = document.querySelector(".button-div");
 
 
 
 // Eliminate Starting Content, Begin Quiz
 var beginQuizHandler = function() {
-    mainDivEl.remove();
-    var questionHandler = function() {
-        var questionDivEl = document.createElement("div")
-            questionDivEl.className = "question-container";
-            questionDivEl.innerHTML = "<h2 class='question-name'>" + question[i] + "</h3>";
+    titleEl.remove();
+    rulesEl.remove();
+    buttonEl.remove();
 
-        var answersEl = document.createElement("div");
-            answersEl.innerHTML = "<button class='answer-button'>" + answers[j] + "<button>";
+    var questionTitle = document.createElement("div");
+    questionTitle.innerHTML = "<h2 class='question-name'>" + questions[0].question + "</h3>";
+    containerEl.appendChild(questionTitle);
 
-        questionDivEl.appendChild(answersEl);
+    for (let i = 0; i < questions[0].answers.length; i++) {
+        var answerButtons = document.createElement("div");
+    answerButtons.className = "buttons";
+    answerButtons.innerHTML += "<button class='quiz-button'>" + questions[0].answers[i] + "</button>";
+    questionTitle.append(answerButtons);
     }
-    return questionHandler;
+
+    
+   
+   
+   
+   
+   
+   
+   
+   
+    // var answerButtons = document.getElementByClassName("button-div").innerHTML += "<button class='begin-quiz'>" + questions[0].answers[i] + "</button>";
+    
+ 
+    
+    
+   
+    // for (let i = 0; i < questions.length; i++) {
+    //     var questionDivEl = document.createElement("div")
+    //     questionDivEl.className = "question-container";
+    //     questionDivEl.innerHTML = "<h2 class='question-name'>" + questions[i].question + "</h2>";
+    //     mainEl.appendChild(questionDivEl);
+
+
+        
+        // for (let j = 0; j < answers.length; j++) {
+        //     var answersEl = document.createElement("div");
+        //     answersEl.innerHTML = "<button class='answer-button'>" + questions[j].answers + "<button>";
+
+        // questionDivEl.appendChild(answersEl);
+        //     if (answers === correct) {
+        //         window.alert("Correct!")
+        //     } else {
+        //         window.alert("Wrong.")
+        //             // timer -= 1500        
+        //     }
+        // }
+        
+        
+    // }
 };
-
-// var beginQuizHandler = function(event) {
-//     var gameBegin = () => {
-//         mainEl.hide; 
-//         for (let i = 0; i < question.length; i++) {
-//             for (let j = 0; j < answers.length; j++) {
-//                 if (answers === correct) {
-//                      window.alert("Correct!")
-//                  } else {
-//                      window.alert("Wrong.")
-//                          timer -= 1500        
-//                    }
-//             }
-//         }
-//     }
-// };
-
-
-
 
 
 // Event Listener to begin quiz when button is clicked
